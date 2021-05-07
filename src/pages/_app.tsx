cbf1f2ecfa9583a0fd1e749344d6eba3850983ad
Pedro Paulo <Pedrinho_fsf@hotmail.com>
2021-05-06 21:40:40 -0300

    Resolvendo bugs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,23 +2,19 @@ import "../styles/globals.scss";
 
 import { Header, Nav, SubHeader } from '../components';
 
-import { MainProvider } from '../Provider/mainProvider';
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <MainProvider>
-        <div>
-          <Header/>
+      <div>
+        <Header/>
+      </div>
+      <div id="container">
+        <Nav/>
+        <div id="mainHeader">
+          <SubHeader/>
+          <Component {...pageProps} />
         </div>
-        <div id="container">
-          <Nav/>
-          <div id="mainHeader">
-            <SubHeader/>
-            <Component {...pageProps} />
-          </div>
-        </div>
-      </MainProvider>
+      </div>
     </>
   ) 
   
